7399c890c7d8151951717e954e7de8dfd4ee76a4
Lovely-s03 <supriyalovely0306@gmail.com>
2025-02-03 11:19:10 +0530

    this

---
+++ app/COMMON/ContactForm3.js
@@ -0,0 +1,89 @@
+import React,{useState} from 'react';
+const ContactForm3 = () => {
+    const [user1, setUser1] = useState({
+        name4: "",
+        email4: "",
+        requirement4: "",
+      });
+
+      const handleChange = (e) => {
+        setUser1({ ...user1, [e.target.name]: e.target.value });
+      };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        // https://kushel-digi.onrender.com
+        // http://localhost:5000
+    
+        const res = await fetch("https://backend.kusheldigi.com/contact3", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(user1),
+        });
+        const data = await res.json();
+        console.log(data);
+        alert(data.message);
+        setUser1({
+          name4: "",
+          email4: "",
+          requirement4: ""
+        });
+      };
+  return (
+     <>
+         <div className="exam_form">
+              <h3>Build Your Team</h3>
+              <p>Hire india’s best pre-vetted software developers on -demand</p>
+              <form onSubmit={handleSubmit}>
+                <div className="form_exam w-full">
+                  <label htmlFor="name4">Your Name</label>
+                  <input
+                    className="w-full mt-3"
+                    type="text"
+                    placeholder="Enter your name"
+                    required
+                    name='name4'
+                    value={user1.name4}
+                    onChange={handleChange}
+                    id='name4'
+                  />
+                </div>
+                <div className="form_exam w-full mt-5">
+                  <label htmlFor="email4">Your Email</label>
+                  <input
+                    className="w-full mt-3"
+                    type="email"
+                    placeholder="Enter your email"
+                    required
+                    name='email4'
+                    value={user1.email4}
+                    onChange={handleChange}
+                    id='email4'
+                  />
+                </div>
+                <div className="form_exam w-full mt-5">
+                  <label htmlFor="requirement4">Your Requirement</label>
+                  <textarea
+                    className="w-full mt-3"
+                    placeholder="Enter your Requirement"
+                    cols="30"
+                    rows="10"
+                    required
+                    name='requirement4'
+                    value={user1.requirement4}
+                    onChange={handleChange}
+                    id='requirement4'
+                  ></textarea>
+                </div>
+                <div className="form_exam_btn">
+                  <button>Submit</button>
+                </div>
+              </form>
+            </div>
+     </>
+  )
+}
+
+export default ContactForm3
---
+++ app/COMMON/ServiceCommon.js
@@ -0,0 +1,132 @@
+import React,{useEffect,useState} from 'react'
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+// import { NavLink } from 'react-router-dom';
+import Link from 'next/link';
+const ServiceCommon = () => {
+    const [perPage, setPerPage] = useState(3);
+  useEffect(() => {
+      if (window.matchMedia("(max-width: 1350px)").matches) {
+          setPerPage(3);
+      }
+      if (window.matchMedia("(max-width: 750px)").matches) {
+          setPerPage(1);
+      }
+  }, []);
+  return (
+     <>
+            <div className="others_nat_serv dine-1234">
+                <h3 className='ot_ser'>Other Services</h3>
+                <hr className='ot_line' />
+                <div className="nat_card">
+                    <Splide
+                        className="w-full"
+                        aria-label="Our Services"
+                        options={{
+                            perPage: perPage,
+                            perMove: 1,
+                            autoplay: true,
+                            pauseOnHover: true,
+                            type: "loop",
+                            interval: 3000,
+                            drag: true,
+                            gap: 1
+                        }}
+                    >
+                        <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1724914748/nativeSer1.cbab4fca37286881546c_pbzjvd.png" alt="E-commerce Solutions" title='E-commerce Solutions' />
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>E-commerce Solutions</h3>
+                                    <p>Teams at fast-growing companies use Superside to get quality graphic design done at scale. </p>
+                                   <Link href="/ecommerce-development"><span>Read More</span></Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1724914747/nativeSer2.a1a157ff9a3b4df1f1bf_gvgzqi.png" alt="UI/UX Design"  title='UI/UX Design'/>
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>UI/UX Design</h3>
+                                    <p>redesign, rebuild and redefine user
+                                        experiences and craft interactions like no
+                                        other with our best-in-class design services.</p>
+                                   <Link href="/ui-ux-design"><span>Read More</span></Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1724914747/nativeSer3.2a73bb3aa0bc26c1e106_tdtnil.png" alt="Mobile App Development" title='Mobile App Development' />
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>Mobile App Development</h3>
+                                    <p>We design custom dynamic mobile apps using cutting-edge technologies to help you transform your business.</p>
+                                   <Link href="/mobile-application-development"><span>Read More</span></Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+
+                        <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1724914747/pjkn.ad23c6892c44ed733e6f_qyclmb.jpg" alt="Web Development" title='Web Development' />
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>Web Development</h3>
+                                    <p>Our web professionals create strong, durable, and bespoke websites that set you apart from the competition.</p>
+                                   <Link href="/web-development"><span>Read More</span></Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1724914747/nativeSer5.18138177fb65e4a7f9ea_rdaczb.png" alt="Graphic Design" title='Graphic Design' />
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>Graphic Design</h3>
+                                    <p>Teams at fast-growing companies use Superside to get quality graphic design done at scale.</p>
+                                  <Link href="/graphic-design"><span>Read More</span></Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1724914747/nativeSer6.2d4d3a21f476cb0f2c92_qawkke.png" alt="Digital Marketing" title='Digital Marketing' />
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>Digital Marketing</h3>
+                                    <p>Drive sustainable growth, increase customer acquisition, and high website traffic with our online tools.</p>
+                                   <Link href="/digital-marketing-agency"><span>Read More</span></Link>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                        {/* <SplideSlide>
+                            <div className="nat_box">
+                                <div className="nat_img">
+                                    <img src={nativeSer7} alt="baty" />
+                                </div>
+                                <div className="nat_heading">
+                                    <h3>AR/VR</h3>
+                                    <p>We assist clients to employ AR and VR technologies to captivate audiences and multiple ROI.</p>
+                                    <span>Read More</span>
+                                </div>
+                            </div>
+                        </SplideSlide> */}
+                    </Splide>
+
+                </div>
+            </div>
+
+     </>
+  )
+}
+
+export default ServiceCommon
