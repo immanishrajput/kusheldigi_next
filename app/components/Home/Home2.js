2650207054e9d67a1ee680288152da05ed9d7674
Lovely-s03 <supriyalovely0306@gmail.com>
2025-03-05 12:50:57 +0530

    this

--- app/components/Home/Home2.js
+++ app/components/Home/Home2.js
@@ -6,7 +6,8 @@ const Home2 = () => {
     <div className='home2Div'>
       <p>Award-winning Agency Partner</p>
       <div>
-        <img src='https://res.cloudinary.com/dgif730br/image/upload/v1741089039/asit-sir-C-01_vj8heh.png' alt='certified' height={170} width={170}></img>
+        <a href="https://partners.bigcommerce.com/directory/partner/1983347/kushel-digi-solutions">
+        <img src='https://res.cloudinary.com/dgif730br/image/upload/v1741089039/asit-sir-C-01_vj8heh.png' alt='certified' height={170} width={170}></img></a>
       </div>
       <div><img src='https://res.cloudinary.com/dgif730br/image/upload/v1741089039/asit-sir-B-01_rytq2t.png' alt='partner' height={170} width={170}></img> </div>
     </div>
