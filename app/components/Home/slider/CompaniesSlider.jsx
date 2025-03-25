dd10639c9678f045f2722f0e6667746d3e7a7add
Inderpal1000 <inderpal.kusheldigi@gmail.com>
2025-03-26 00:20:42 +0530

    slider added

--- app/components/Home/slider/CompaniesSlider.jsx
+++ app/components/Home/slider/CompaniesSlider.jsx
@@ -1,50 +1,36 @@
-import React from "react";
+'use client'
+
+import React, { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './NewSlider.css';
 import Slider from "react-slick";
 
-
 const CompaniesSlider = () => {
+    const [autoplaySpeed, setAutoplaySpeed] = useState(1); 
 
-const scrollSettings = {
-    dots: true,
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAutoplaySpeed(4000); 
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const commonSettings = {
+    dots: false,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 1500,
-    autoplaySpeed: 1700,
+    speed: 4000,
+    autoplaySpeed: autoplaySpeed,
     cssEase: "linear",
     waitForAnimate: false,
-    pauseOnHover: false
-};
+    pauseOnHover: false,
+    initialSlide: 1,
+  };
 
-const scrollSettings2 = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-    waitForAnimate: false,
-    pauseOnHover: false
-};
-
-const scrollSettings3 = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 1800,
-    autoplaySpeed: 1600,
-    cssEase: "linear",
-    waitForAnimate: false,
-    pauseOnHover: false
-};
 
   return (
     <div className="logo-container">
@@ -54,191 +40,29 @@ const scrollSettings3 = {
             Trusted choice of Top <strong>E-commerce</strong> Brands Worldwide
           </p>
         </div>
-        <Slider {...scrollSettings} style={{margin:'10px'}}>
-        <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp18_iscwvs.png`}
-                width="85%"
-                alt="logo"
-              />
+
+        <Slider {...commonSettings} className="custom-slider">
+          {["sp18_iscwvs.png", "sp19_lg8ebe.png", "sp20_xvpwrq.png", "sp1_tlo2jq.png", "sp2_udzoec.png", "sp3_kriscj.png", "sp6_oqjqdl.png", "sp4_mjfpjw.png"].map((img, index) => (
+            <div key={index} className="slide-item">
+              <img src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/${img}`} alt="logo" />
             </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp19_lg8ebe.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp20_xvpwrq.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-        
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp1_tlo2jq.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp2_udzoec.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp3_kriscj.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp6_oqjqdl.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp4_mjfpjw.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
+          ))}
         </Slider>
 
-
-        <Slider {...scrollSettings2} style={{margin:'20px 0px'}}>
-        <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp5_sk9ico.png`}
-                width="85%"
-                alt="logo"
-              />
+        <Slider {...commonSettings} rtl className="custom-slider">
+          {["sp5_sk9ico.png", "sp7_ludrbx.png", "sp8_neiqlr.png", "sp15_udrdig.png", "sp16_romtvs.png", "sp17_awm1ho.png", "sp18_iscwvs.png", "sp19_lg8ebe.png"].map((img, index) => (
+            <div key={index} className="slide-item">
+              <img src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/${img}`} alt="logo" />
             </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp7_ludrbx.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp8_neiqlr.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp15_udrdig.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp16_romtvs.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp17_awm1ho.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp18_iscwvs.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp19_lg8ebe.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp20_xvpwrq.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
+          ))}
         </Slider>
 
-
-        <Slider {...scrollSettings3} style={{margin:'10px'}}>
-        <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp3_kriscj.png`}
-                width="85%"
-                alt="logo"
-              />
+        <Slider {...commonSettings} className="custom-slider">
+          {["sp3_kriscj.png", "sp6_oqjqdl.png", "sp4_mjfpjw.png", "sp10_b2yb2j.png", "sp11_wd33xt.png", "sp12_wdyncp.png", "sp13_uplo8b.png", "sp14_t0ac1k.png"].map((img, index) => (
+            <div key={index} className="slide-item">
+              <img src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/${img}`} alt="logo" />
             </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp6_oqjqdl.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp4_mjfpjw.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-        <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp10_b2yb2j.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp11_wd33xt.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp12_wdyncp.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp13_uplo8b.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
-            <div>
-            <img
-                src={`https://res.cloudinary.com/dqjbzgksw/image/upload/v1740985644/sp14_t0ac1k.png`}
-                width="85%"
-                alt="logo"
-              />
-            </div>
+          ))}
         </Slider>
       </div>
     </div>
