2c5e6132b597559e62af49317acc73b21b90550e
Lovely-s03 <supriyalovely0306@gmail.com>
2025-04-02 15:20:35 +0530

    bigcommerce

--- app/BigCommerce/page.js
+++ app/BigCommerce/page.js
@@ -7,11 +7,14 @@ import BigBanner7 from './BigBanner7/page'
 import BigBanner8 from './BigBanner8/page'
 import BannerMain from './BannerMain/page'
 import Process from './Process/page'
-
+import Navbar from '../COMMON/Navbar'
 import Banner10 from '../Ecommerce/Banner9/page'
+import Footer from '../COMMON/Footer'
+import BigAskQuestion from '../bigcommerce-development/BigAskQuestion/page'
 const page = () => {
   return (
     <div>
+      {/* <Navbar/> */}
       <BannerMain/>
       <BigBanner8/>
       <BigBanner7/>
@@ -20,6 +23,8 @@ const page = () => {
       <Process/>
       <BigBanner6/>
         <BigBanner4/>
+        <BigAskQuestion/>
+        <Footer/>
     </div>
   )
 }
