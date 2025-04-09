ba31e5144da25dc9bc29ee07a9e22862f2eb5734
Inderpal1000 <inderpal.kusheldigi@gmail.com>
2025-04-09 19:16:14 +0530

    changes

---
+++ app/magento/MagentoBanner/MagentoBanner.jsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from 'react';
+import "./MagentoBanner.css";
+import { FaArrowRightLong } from 'react-icons/fa6';
+import { useRouter } from 'next/navigation';
+
+const MagentoBanner = () => {
+    const navigate = useRouter();
+    const scrollToForm = () => {
+        const formSection = document.getElementById('form-section');
+        if (formSection) {
+          const yOffset = -120; 
+          const y = formSection.getBoundingClientRect().top + window.pageYOffset + yOffset;
+          window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+      };
+  return (
+    <div className='magento-banner-outer-container'>
+        <div className='magento-banner-inner-container'>
+            <div className="magento-banner-left">
+                <h1 className='magento-banner-heading'>Magento Development Services</h1>
+                <div className='magento-banner-para-cont'>
+                <p>Our custom magento website development services are tailored to your specific needs.</p>
+                <p>Your e-commerce site becomes more reliable, fully customizable, and ready for the future with our dedicated team on your side.</p>
+                </div>
+
+                <div className='flex-new-banner-btns'>
+                        <button onClick={scrollToForm} className='new-banner-btn colored'>Talk to a Consultant</button>
+                        <button  onClick={() => navigate.push('/contact-us')}  className='new-banner-btn transparent'>Book a Call  <FaArrowRightLong /></button>
+                </div>
+            </div>
+            <div className="magento-banner-right">
+                <img src="https://res.cloudinary.com/dd9tagtiw/image/upload/v1744202442/63986d38ae551802a6ffe22c_full_version_checkout_tqy9hv.gif" alt="magnto-banner" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MagentoBanner
