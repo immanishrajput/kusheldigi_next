8960a0084620818938671fd6b71da00b903f202f
Lovely-s03 <supriyalovely0306@gmail.com>
2025-04-16 12:44:43 +0530

    magento

--- app/magento/TechWeUse/TechWeUse.jsx
+++ app/magento/TechWeUse/TechWeUse.jsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { MdOutlineNavigateNext } from "react-icons/md";
 import { GrFormPrevious } from "react-icons/gr";
+import { Autoplay } from 'swiper/modules';
 
 
 function SampleNextArrow(props) {
@@ -61,6 +62,9 @@ const TechWeUse = () => {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+           cssEase: "linear",
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
         responsive: [
