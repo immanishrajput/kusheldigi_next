501fdf3502c8f13ea9cbabbfa57c5d2ea67b622e
Rajatbora869 <rajatbora869@gmail.com>
2025-04-28 21:59:29 +0530

    this

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,10 +1,21 @@
 "use client";
 import React, { useEffect } from 'react';
-import PortfolioBanner from './PortfolioBanner/page';
-import PortfolioCard from './PortfolioCard/paje';
+import PortfolioBanner from './PortfolioBanner/page'; 
+import PortfolioCard from './PortfolioCard/paje';     
 import Navbar from '../COMMON/Navbar';
 import Footer from '../COMMON/Footer';
 import HomeForm from '../components/Home/HomeForm';
+
+const phoneNumber = "9045301702";
+
+const whatAppHandler = () => {
+  const whatsappUrl = `https://wa.me/${phoneNumber}`;
+  window.open(whatsappUrl, "_blank");
+};
+
+const callHandler = () => {
+  window.location.href = `tel:${phoneNumber}`;
+};
 
 const Page = () => {
   useEffect(() => {
@@ -30,6 +41,15 @@ const Page = () => {
       <PortfolioCard />
       <HomeForm />
       <Footer />
+
+      <div className="whtsApBtns">
+        <button onClick={whatAppHandler}>
+          <img className="what-image-universal" src='https://res.cloudinary.com/dd9tagtiw/image/upload/v1738990311/whatsapp_eohddq.png' alt="whatsApp-kusheldigi" title="whatsApp-kusheldigi" />
+        </button>
+        <button onClick={callHandler}>
+          <img src='https://res.cloudinary.com/dd9tagtiw/image/upload/v1740480725/telephone_h8clxy.png' alt="call-icon" title="call-icon" />
+        </button>
+      </div>
     </div>
   );
 };
