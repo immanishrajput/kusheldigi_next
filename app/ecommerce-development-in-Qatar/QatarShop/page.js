5eada23c31f6776c0f9a7450a60864ff7a84cd7e
immanishrajput <manishrajput78380@gmail.com>
2025-06-23 11:18:41 +0530

    new-sec

---
+++ app/ecommerce-development-in-Qatar/QatarShop/page.js
@@ -0,0 +1,37 @@
+"use client"
+import React from 'react'
+import "./londonshop.css"
+function QatarShop() {
+  return (
+    <div>
+       <section className="Conversions1-wrapper">
+        <div className="Conversions1-container">
+          <div className="Conversions1-left">
+            <h2>
+            Turn Clicks Into Customers with <br/> Smart E-Commerce Development
+            </h2>
+            <ul className="Conversions1-dashlist">
+  <li><span>–</span> Turn casual visits into sales with fast, high-impact online store tools.</li>
+  <li><span>–</span> Help shoppers find what they want with clear, simple menus.</li>
+  <li><span>–</span> Draw in more buyers using the SEO features already built in.</li>
+  <li><span>–</span> Let smart filters and predictive search put products front and center.</li>
+  <li><span>–</span> Keep visitors returning with a site that works perfectly on mobile.</li>
+</ul>
+            <p>
+            An agile ecommerce platform can turn browsers into buyers- let's create yours.
+            </p>
+          </div>
+          <div className="Conversions1-right">
+            <img
+              src="https://res.cloudinary.com/dbcmdtr3r/image/upload/v1750231672/EcomNewbanner_1_mon3ra.png"
+              alt="60% shoppers leave bad UX"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default QatarShop;
+  
--- app/ecommerce-development-in-Qatar/page.js
+++ app/ecommerce-development-in-Qatar/page.js
@@ -1,22 +1,23 @@
-import EcommerceQatar from '../components/EcommerceQatar';
+import EcommerceQatar from "../components/EcommerceQatar"; 
+
 export const metadata = {
   openGraph: {
     title: "Top eCommerce Development Company | Kushel Digi Solutions.",
-    description: "Elevate your online store with Kushel Digi expert eCommerce development. Custom, scalable solutions on Shopify, Magento, BigCommerce & more. Boost sales with seamless UX & secure payments.",
+    description:
+      "Elevate your online store with Kushel Digi expert eCommerce development. Custom, scalable solutions on Shopify, Magento, BigCommerce & more. Boost sales with seamless UX & secure payments.",
     url: "https://www.kusheldigi.com/ecommerce-development",
-    type:"website",
-    images: [{ url: "https://res.cloudinary.com/dd9tagtiw/image/upload/v1744463967/mockup-4_h41e2o.png", width: 1200, height: 630 , alt:"kushel" }],
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dd9tagtiw/image/upload/v1744463967/mockup-4_h41e2o.png",
+        width: 1200,
+        height: 630,
+        alt: "kushel",
+      },
+    ],
   },
-}
-
-const QatarEcommerce = ({ notify }) => {
-  
-  return (
-    <> 
-    <EcommerceQatar notify={notify} />
-    </>
-  );
 };
 
-export default QatarEcommerce;
-
+export default function Page({ notify }) {
+  return <EcommerceQatar notify={notify} />;
+}
