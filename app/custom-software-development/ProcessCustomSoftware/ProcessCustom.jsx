f65faf79968627d0d265c33efa726f8088100a1b
Karankartik07 <unknownkartik07@gmail.com>
2025-07-15 12:07:33 +0530

    15july

--- app/custom-software-development/ProcessCustomSoftware/ProcessCustom.jsx
+++ app/custom-software-development/ProcessCustomSoftware/ProcessCustom.jsx
@@ -45,25 +45,25 @@ const Shopify = () => {
                             {
                                 inView && (
                                     <div className="shopify-banner-right-content">
-                                        <div id='shopify-1st-service' className="shopify-banner-services">
+                                        <div id='shopify-1st-service' className="shopify-banner-services ext-class-h3-s">
                                             <h3 className='shopify-banner-1st-service-heading'>Sprint planning</h3>
                                             <p className='shopify-banner-1st-service-para'>Sprint roadmap is a collective planning effort. Team members collaborate to clarify items and ensure shared understanding.</p>
                                         </div>
-                                        <div id='shopify-2nd-service' className="shopify-banner-services">
+                                        <div id='shopify-2nd-service' className="shopify-banner-services ext-class-h3-s">
                                             <h3 className='shopify-banner-2nd-service-heading'>Tech architecture</h3>
                                             <p className='shopify-banner-2nd-service-para'>We break monolithic apps into microservices. Decoupling the code allows teams to move faster and more independently.
                                             </p>
                                         </div>
-                                        <div id='shopify-3rd-service' className="shopify-banner-services">
+                                        <div id='shopify-3rd-service' className="shopify-banner-services ext-class-h3-s">
                                             <h3 className='shopify-banner-3rd-service-heading'>Stand ups & weekly demos
                                             </h3>
                                             <p className='shopify-banner-3rd-service-para'>Stand ups, weekly demos, and weekly reviews make sure everyone is on the same page and can raise their concerns.</p>
                                         </div>
-                                        <div id='shopify-4th-service' className="shopify-banner-services">
+                                        <div id='shopify-4th-service' className="shopify-banner-services ext-class-h3-s">
                                             <h3 className='shopify-banner-4th-service-heading'>Code reviews</h3>
                                             <p className='shopify-banner-4th-service-para'>Code reviews before release help detect issues like memory leaks, file leaks, performance signs, and general bad smells.</p>
                                         </div>
-                                        {/* <div id='shopify-5th-service' className="shopify-banner-services">
+                                        {/* <div id='shopify-5th-service' className="shopify-banner-services ext-class-h3-s">
                                             <h3 className='shopify-banner-5th-service-heading'>Launch & Support</h3>
                                             <p className='shopify-banner-5th-service-para'>After launch, we provide training, maintenance, and continuous support. 
                                             </p>
